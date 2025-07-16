05b59733ee385265d1fe3c76c1fc35e1735d7780
Adelisef33 <AdeliseFerris@gmail.com>
2025-07-16 11:07:37 -0700

    Add Die class for configurable dice representation
    Introduces the `Die` class in the `DiceRoller` namespace, allowing for a configurable number of sides. The constructor validates the number of sides to ensure it is between 1 and 20, throwing an `ArgumentOutOfRangeException` if the validation fails. A property `NumberOfSides` is also included for accessing and modifying the number of sides.

---
+++ DiceRoller/Die.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller;
+
+/// <summary>
+/// Represents a die with a configurable number of sides.
+/// </summary>
+public class Die
+{
+
+    /// <summary>
+    /// Ceates a new instance of the Die class with the specified number of sides.
+    /// </summary>
+    /// <param name="numSides">
+    /// The sides must be more than 0
+    /// </param>
+    public Die(byte numSides)
+    {
+        if (numSides == 0 || numSides > 20)
+        {
+            throw new ArgumentOutOfRangeException(nameof(numSides)
+                ,$"{nameof(numSides)} must be greater than 0 and less than 21");
+        }
+        NumberOfSides = numSides;
+    }
+
+    /// <summary>
+    /// Gets or sets the number of sides of the die.
+    /// </summary>
+    public byte NumberOfSides { get; set; }
+}
